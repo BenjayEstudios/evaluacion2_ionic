2e0d11241c24b68c64b3d3802d50ac553668ad52
Benja <benja1715@gmail.com>
2024-09-15 18:06:08 -0300

    perfil

--- TinderDuoc/src/app/perfil/perfil.page.ts
+++ TinderDuoc/src/app/perfil/perfil.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-perfil',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerfilPage implements OnInit {
 
-  constructor() { }
+  constructor(private navCtrl: NavController) { }
 
   ngOnInit() {
   }
-
+  EditarPerfil(){
+    this.navCtrl.navigateRoot('/editar-perfil')
+  }
+  CompartirPerfil(){
+    this.navCtrl.navigateRoot('/home')
+  }
 }
