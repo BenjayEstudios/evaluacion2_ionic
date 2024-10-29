e69c46a330b360d9891c50b04afe11a3476641e8
bunnybraixen <117122265+bunnybraixen@users.noreply.github.com>
2024-10-29 20:24:18 -0300

    Se agrega persistencia de datos

--- TinderDuoc/src/app/home/home.page.ts
+++ TinderDuoc/src/app/home/home.page.ts
@@ -8,16 +8,33 @@ import { ApiConsumoService } from '../services/api-consumo.service';
 })
 export class HomePage implements OnInit {
   items: any
+  dataStorage:any
   constructor(private apiService: ApiConsumoService) { }
 
   ngOnInit() {
 
-    this.apiService.obtenerDatos().subscribe((Root) => {
-      console.log(Root)
-      this.items = Root
+    this.dataStorage=localStorage.getItem('data')
 
 
-    })
+    if(this.dataStorage){
+      console.log('datos desde localstorage')
+      this.items=JSON.parse(this.dataStorage)
+
+    }else{
+
+      this.apiService.obtenerDatos().subscribe((Root) => {
+      console.log('datos desde API')
+        console.log(Root)
+        this.items = Root
+        localStorage.setItem('data',JSON.stringify(this.items))
+
+  
+  
+      })
+
+
+    }
+    
 
   }
 
