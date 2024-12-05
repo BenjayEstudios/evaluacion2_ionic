18cbdb3db4a6d6fcaebe14902d6bc70f4020b705
BenjayEstudios <be.gonzalezp@duocuc.cl>
2024-12-04 23:57:04 -0300

    validacion home prueba unitaria

--- TinderDuoc/src/app/home/home.page.spec.ts
+++ TinderDuoc/src/app/home/home.page.spec.ts
@@ -1,16 +1,27 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { IonicModule } from '@ionic/angular';
+import { IonicModule, NavController } from '@ionic/angular';
 
 import { HomePage } from './home.page';
+// import { ApiConsumoService } from '../services/api-consumo.service';
+import { of } from 'rxjs';
+import { ApiConsumoService } from '../services/api-consumo.service';
+import { mock } from '../mock/apiConsumo.mock';
+
 
 describe('HomePage', () => {
   let component: HomePage;
   let fixture: ComponentFixture<HomePage>;
 
+  // 
+
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [HomePage],
-      imports: [IonicModule.forRoot()]
+      imports: [IonicModule.forRoot()],
+      providers: [{
+        provide:ApiConsumoService, useValue:mock
+      }]
     }).compileComponents();
 
     fixture = TestBed.createComponent(HomePage);
